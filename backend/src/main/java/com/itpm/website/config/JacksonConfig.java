faa2dfbb52cdb295316c4dc5f65b34842fd1841d
Anuradha-dol <anuradha123dk@gmail.com>
2026-04-18 13:29:24 +0530

    fix error in backend jacksonConfig

--- backend/src/main/java/com/itpm/website/config/JacksonConfig.java
+++ backend/src/main/java/com/itpm/website/config/JacksonConfig.java
@@ -3,14 +3,14 @@ package com.itpm.website.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return Jackson2ObjectMapperBuilder.json().build();
+        return JsonMapper.builder().findAndAddModules().build();
     }
 }
